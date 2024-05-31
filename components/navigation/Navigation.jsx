575b09fd76f456ab49123f048897265c2eb8a943
e1156439 <156684264+e1156439@users.noreply.github.com>
2024-05-31 17:06:10 +0800

    Updated Navigation.jsx

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screen/LoginScreen';
-import HomeScreen from './screen/HomeScreen';
+import LoginScreen from '../app/screen/LoginScreen';
+import HomeScreen from '../app/screen/HomeScreen';
 
 const Stack = createStackNavigator();
 
